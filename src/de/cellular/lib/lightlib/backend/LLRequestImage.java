fdf906126196cae5bc251a18b2ae3e10447e4b51
Xinyue Zhao <czhao@cellular.de>
2012-07-05 11:27:08 +0200

    Add parent class of LLRequestImage. When crashing at loading bitmap, try to create load as local file and decode bitmap with options.

--- src/de/cellular/lib/lightlib/backend/LLRequestImage.java
+++ src/de/cellular/lib/lightlib/backend/LLRequestImage.java
@@ -16,30 +16,33 @@
 
 package de.cellular.lib.lightlib.backend;
 
+import java.io.File;
 import java.io.IOException;
 
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Handler;
-import android.util.DisplayMetrics;
-import android.view.WindowManager;
-
 import de.cellular.lib.lightlib.log.LLL;
 import de.cellular.lib.lightlib.utils.UIUtils;
 
-public class LLRequestImage extends LLRequest
+public class LLRequestImage extends LLRequestFile
 {
-    private DisplayMetrics  metrics;
-
     public static final int REQUEST_IMAGE_SUCCESSED = 0x37;
-    public static final int REQUEST_IMAGE_FAILED  = 0x38;
+    public static final int REQUEST_IMAGE_FAILED    = 0x38;
 
-    private LLRequestImage( Context _context, Handler _handler, Method _method ) {
+    public static class RequestedSize
+    {
+        public int reqWidth;
+        public int reqHeight;
+    }
+
+    private RequestedSize mReqSize;
+
+    private LLRequestImage( Context _context, Handler _handler, Method _method, RequestedSize _reqSize ) {
         super( _context, _handler, _method );
-        metrics = new DisplayMetrics();
-        ((WindowManager) _context.getSystemService( Context.WINDOW_SERVICE )).getDefaultDisplay().getMetrics(
-                metrics );
+        mReqSize = _reqSize;
     }
 
     @Override
@@ -48,8 +51,8 @@ public class LLRequestImage extends LLRequest
             LLImageResponse ret = new LLImageResponse( _r );
             readStreamToBitmap( ret );
             LLL.i( ":) Reading Bitmap successfully." );
-            
-            onResponse( REQUEST_IMAGE_SUCCESSED, ret ); 
+
+            onResponse( REQUEST_IMAGE_SUCCESSED, ret );
         }
         catch( Exception _e )
         {
@@ -57,8 +60,8 @@ public class LLRequestImage extends LLRequest
             try {
                 _r.release();
             }
-            catch( IOException _e1 ) { 
-                LLL.e( ":( Failed to release res:" + _e.toString() );  
+            catch( IOException _e1 ) {
+                LLL.e( ":( Failed to release res:" + _e.toString() );
             }
             // Info UI that it be failed.
             if( mHandler != null ) {
@@ -69,15 +72,68 @@ public class LLRequestImage extends LLRequest
 
     private void readStreamToBitmap( LLImageResponse _r ) throws IOException {
         if( _r.getInputStream() != null ) {
-            // Create a bitmap from data.
             Bitmap retBp = BitmapFactory.decodeStream( _r.getInputStream() );
             if( retBp != null ) {
-                // Scale the image and cache it.
-                retBp = doScalingImage( retBp );
-                LLImageCache.setSendungImage( _r.getUrlStr(), retBp );
-                _r.setBitmap( retBp );
+                LLL.d( ":) A Bitmap has been decoded successfully." );
+            }
+            else {
+                // --------------------------------------------------------------------------------------------------------------------
+                // The raw bitmap could be too big, so that we can't decode it directly.
+                // It should be loaded by scaling down after being downloaded as a temp file.
+                // see and helper:
+                // http://developer.android.com/intl/zh-CN/training/displaying-bitmaps/load-bitmap.html#load-bitmap
+                // --------------------------------------------------------------------------------------------------------------------
+                LLL.d( ":| A Bitmap has been decoded failed, try to adjust a new one." );
+                File tmpFile = createOutputFile( _r, Uri.parse( _r.getUrlStr() ).getLastPathSegment() );
+                final BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inJustDecodeBounds = true;
+                retBp = BitmapFactory.decodeFile( tmpFile.getAbsolutePath(), options );
+                if( retBp != null ) {
+                    LLL.d( ":) A new Bitmap(from temp file) has been decoded successfully." );
+                }
+                else {
+                    options.inSampleSize = calculateInSampleSize( options, mReqSize.reqWidth, mReqSize.reqHeight );
+                    options.inJustDecodeBounds = false;
+                    retBp = BitmapFactory.decodeFile( tmpFile.getAbsolutePath(), options );
+                    if( retBp != null ) {
+                        LLL.d( ":) A new resized Bitmap(from temp file) has been decoded successfully." );
+                    }
+                    else{
+                        LLL.e( ":( Give up! The Bitmap can't be decoded definitly." ); 
+                    }
+                }
+            }
+            // Scale the image and cache it.
+            retBp = doScalingImage( retBp );
+            LLImageCache.setSendungImage( _r.getUrlStr(), retBp );
+            _r.setBitmap( retBp );
+        }
+    }
+
+    /**
+     * Get new size of being produced bitmap
+     * 
+     * @param options
+     * @param reqWidth
+     * @param reqHeight
+     * @see http://developer.android.com/intl/zh-CN/training/displaying-bitmaps/load-bitmap.html#load-bitmap
+     * @return inSampleSize
+     */
+    public static int calculateInSampleSize( BitmapFactory.Options options, int reqWidth, int reqHeight ) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if( height > reqHeight || width > reqWidth ) {
+            if( width > height ) {
+                inSampleSize = Math.round( (float) height / (float) reqHeight );
+            }
+            else {
+                inSampleSize = Math.round( (float) width / (float) reqWidth );
             }
         }
+        return inSampleSize;
     }
 
     public static LLRequestImage start(
@@ -85,8 +141,9 @@ public class LLRequestImage extends LLRequest
             Handler _handler,
             Method _method,
             String _url,
+            RequestedSize _reqSize,
             String _someCookies ) {
-        LLRequestImage r = new LLRequestImage( _context, _handler, _method );
+        LLRequestImage r = new LLRequestImage( _context, _handler, _method, _reqSize );
         r.execute( createHttpClient( false ), _url, _someCookies );
         return r;
     }
@@ -102,11 +159,10 @@ public class LLRequestImage extends LLRequest
         if( _bitmap.getHeight() > _bitmap.getWidth() )
             return UIUtils.scaleImageHW(
                     _bitmap,
-                    metrics.widthPixels );
+                    mReqSize.reqWidth );
         else
             return UIUtils.scaleImageWH(
                     _bitmap,
-                    metrics.widthPixels );
-
+                    mReqSize.reqWidth );
     }
 }
